583bea2734fc2a9446fe32d52ec3f8e0d7fe98c3
Daniel Marcotte <daniel@marcottemotors.io>
2022-03-23 14:50:07 -0700

    Get our build playing nice with Apple silicon
    Upgrade Kotlin and fix a node compatibility issue to get our project
    playing nice one the new Apple cpus.
    
    Note: this also checks in a `yarn.lock` generated by KotlinJS describing
    the dependencies used internally by Kotlin.  This is new as of the
    Kotlin (`1.6.10`) that we upgrade to in this commit.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
 }
 
 repositories {
