bdd8a8602534ef64db42723f2cc8e069a9170989
Daniel Marcotte <daniel@marcottemotors.io>
2024-01-12 17:36:11 -0800

    Enhance JGit JsonTestSuite checkout, add tests
    fc6388618e92 introduced a cross-platform JGit approach to checking out
    the [JSONTestSuite](https://github.com/nst/JSONTestSuite) tests, which
    is _great_, though it accidentally dropped some features of the
    previous implementation, specifically:
    
    - only clone JSONTestSuite if the checkout does not already exist
    - ensure we never run a build on a dirty checkout of JSONTestSuite,
    (because wow would that would be nightmare to troubleshoot)
    
    It makes sense these were missed in fc6388618e92 because these features
    were untested and hiding in the `ensure_suite.sh` bash script that
    fc6388618e92 deleted.
    
    Reintroduce these desired features and ensure good test coverage to
    highlight and ensure them going forward.

--- buildSrc/src/main/kotlin/GenerateJsonTestSuiteTask.kt
+++ buildSrc/src/main/kotlin/GenerateJsonTestSuiteTask.kt
@@ -5,6 +5,12 @@ import java.io.File
 import java.nio.file.Paths
 
 /**
+ * The Git SHA in the [JSONTestSuite](https://github.com/nst/JSONTestSuite) we currently test against.
+ * This can be updated if/when we want to pull in newer tests from that project.
+ */
+private const val testSuiteSHA = "d64aefb55228d9584d3e5b2433f720ea8fd00c82"
+
+/**
  * This task exposes [JsonTestSuiteGenerator] to our Gradle build, ensuring the task's
  * [inputs and outputs](https://docs.gradle.org/current/userguide/more_about_tasks.html#sec:task_inputs_outputs)
  * are properly defined so that we support incremental builds (and so that, for instance, the task re-runs
@@ -12,7 +18,12 @@ import java.nio.file.Paths
  */
 open class GenerateJsonTestSuiteTask : DefaultTask() {
     private val jsonTestSuiteGenerator =
-        JsonTestSuiteGenerator(project.projectDir.toPath(), Paths.get("src/commonTest/kotlin/"), "org.kson.parser.json.generated")
+        JsonTestSuiteGenerator(
+            testSuiteSHA,
+            project.projectDir.toPath(),
+            Paths.get("src/commonTest/kotlin/"),
+            "org.kson.parser.json.generated"
+        )
 
     init {
         // ensure we're out of date when/if the repo of test source files is deleted
