fe8d249525fd6538327919e34a68766724b0b368
Daniel Marcotte <daniel@marcottemotors.io>
2024-10-14 12:56:51 -0700

    Fix stale code reference
    Fix the broken code link in the docs of `KsonBackspaceHandlerDelegateTest`

--- tooling/jetbrains/src/test/kotlin/org/kson/jetbrains/editor/KsonBackspaceHandlerDelegateTest.kt
+++ tooling/jetbrains/src/test/kotlin/org/kson/jetbrains/editor/KsonBackspaceHandlerDelegateTest.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.actionSystem.IdeActions
 class KsonBackspaceHandlerDelegateTest : KsonEditorActionTest() {
     /**
      * Validate quote pairs are deleted when they are empty and the caret is between them
-     * This is the inverse operation to what is tested in [org.kson.jetbrains.editor.KsonQuoteHandlerTest.testAutoInsert]
+     * This is the inverse operation to what is tested in [org.kson.jetbrains.editor.KsonQuoteMatcherTest.testAutoInsert]
      */
     fun testDeleteEmptyQuotePairs() {
         withConfigSetting(ConfigProperty.AUTOINSERT_PAIR_QUOTE, true) {
