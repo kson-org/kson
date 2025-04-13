dabd21a23786c4d89bde9e4a02514fd46d6267df
holodorum <bartdubbeldam2000@gmail.com>
2025-04-13 17:21:00 +0200

    Enhance parser with additional PsiElements
    Add `KsonEmbedBlock` and `KsonEmbedContent`
    as Psi elements. This change prepares for language injection.
    
    # Conflicts:
    #	tooling/jetbrains/src/test/resources/testData/parser/ManyConstructs.txt

---
+++ tooling/jetbrains/src/main/kotlin/org/kson/jetbrains/psi/KsonEmbedContent.kt
@@ -0,0 +1,5 @@
+package org.kson.jetbrains.psi
+
+import com.intellij.lang.ASTNode
+
+open class KsonEmbedContent(node: ASTNode) : KsonPsiElement(node)
--- tooling/jetbrains/src/main/kotlin/org/kson/jetbrains/psi/KsonPsiElement.kt
+++ tooling/jetbrains/src/main/kotlin/org/kson/jetbrains/psi/KsonPsiElement.kt
@@ -3,4 +3,4 @@ package org.kson.jetbrains.psi
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 
-class KsonPsiElement(node: ASTNode) : ASTWrapperPsiElement(node)
+open class KsonPsiElement(node: ASTNode) : ASTWrapperPsiElement(node)
