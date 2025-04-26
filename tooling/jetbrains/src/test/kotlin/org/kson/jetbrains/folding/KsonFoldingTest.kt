484388353e78d28fe23eb54a63c521d6948d7a51
Bart Dubbeldam <bartdubbeldam2000@gmail.com>
2025-04-26 11:28:34 +0200

    Add code folding support for Kson files
    Implemented a `KsonFoldingBuilder` to enable code folding for lists,
    objects, and embedded blocks in Kson files.

---
+++ tooling/jetbrains/src/test/kotlin/org/kson/jetbrains/folding/KsonFoldingTest.kt
@@ -0,0 +1,41 @@
+package org.kson.jetbrains.folding
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class KsonFoldingTest : BasePlatformTestCase() {
+    override fun getTestDataPath(): String = "src/test/resources/testData/folding"
+
+    override fun setUp() {
+        super.setUp()
+        myFixture.testDataPath = testDataPath
+    }
+
+    fun testDashListFolding() {
+        doTest()
+    }
+
+    fun testBracketListFolding() {
+        doTest()
+    }
+
+    fun testAngleListFolding() {
+        doTest()
+    }
+
+    fun testEmbedBlockFolding() {
+        doTest()
+    }
+
+    fun testSingleLineShouldNotFold() {
+        doTest()
+    }
+
+    fun testObjectFolding() {
+        doTest()
+    }
+
+    private fun doTest() {
+        val testName = getTestName(true)
+        myFixture.testFolding("$testDataPath/$testName.kson")
+    }
+} 
