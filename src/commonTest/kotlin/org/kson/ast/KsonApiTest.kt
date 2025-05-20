a7abc4400c9680662cb9cae65a30b0937e1407c4
Daniel Marcotte <daniel@marcottemotors.io>
2025-05-19 17:24:22 -0700

    Improve naming around the AST API classes
    `AstApi` is now `KsonApi` and its Kson tree node subclasses are now
    named simply for the Kson value or element they represent so this is
    a clean and hopefully clear and intuitive interface on Kson-parsed
    data

--- src/commonTest/kotlin/org/kson/ast/KsonApiTest.kt
+++ src/commonTest/kotlin/org/kson/ast/KsonApiTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class AstApiTest {
+class KsonApiTest {
     @Test
     fun testAstApiComprehensive() {
         // Exercise all AstApi node types with some non-trivial nesting
@@ -46,37 +46,34 @@ class AstApiTest {
         val parseResult = Kson.parseToAst(source)
         assertTrue(!parseResult.hasErrors(), "Parse should succeed without errors")
         
-        val astApi = parseResult.ast!!.toAstApi()
-        assertTrue(astApi is KsonRootApi, "Root should be KsonRootApi")
-        
-        val objectNode = astApi.rootNode
-        assertTrue(objectNode is ObjectNodeApi, "Root node should be ObjectNodeApi")
+        val objectNode = parseResult.ast!!.toKsonApi()
+        assertTrue(objectNode is KsonObject, "Root node should be ObjectNodeApi")
 
        val properties = objectNode.properties.associateBy { it.name.value }
 
         // Verify string value
         val stringProp = properties["stringVal"]!!
-        assertTrue(stringProp.value is StringNodeApi)
-        assertEquals("hello world", (stringProp.value as StringNodeApi).value)
+        assertTrue(stringProp.value is KsonString)
+        assertEquals("hello world", (stringProp.value as KsonString).value)
 
         // Verify number value
         val numberProp = properties["numberVal"]!!
-        assertTrue(numberProp.value is NumberNodeApi)
-        assertEquals(42.5, ((numberProp.value as NumberNodeApi).value as NumberParser.ParsedNumber.Decimal).value)
+        assertTrue(numberProp.value is KsonNumber)
+        assertEquals(42.5, ((numberProp.value as KsonNumber).value as NumberParser.ParsedNumber.Decimal).value)
 
         // Verify boolean value
         val booleanProp = properties["booleanVal"]!!
-        assertTrue(booleanProp.value is BooleanNodeApi)
-        assertEquals(true, (booleanProp.value as BooleanNodeApi).value)
+        assertTrue(booleanProp.value is KsonBoolean)
+        assertEquals(true, (booleanProp.value as KsonBoolean).value)
 
         // Verify null value
         val nullProp = properties["nullVal"]!!
-        assertTrue(nullProp.value is NullNodeApi)
+        assertTrue(nullProp.value is KsonNull)
 
         // Verify embed block
         val embedProp = properties["embedBlock"]!!
-        assertTrue(embedProp.value is EmbedBlockNodeApi)
-        val embedBlock = embedProp.value as EmbedBlockNodeApi
+        assertTrue(embedProp.value is EmbedBlock)
+        val embedBlock = embedProp.value as EmbedBlock
         assertEquals("", embedBlock.embedTag)
         assertEquals("""
             This is an embed block
@@ -86,47 +83,47 @@ class AstApiTest {
 
         // Verify nested object
         val nestedObjectProp = properties["nestedObject"]!!
-        assertTrue(nestedObjectProp.value is ObjectNodeApi)
-        val nestedObject = nestedObjectProp.value as ObjectNodeApi
+        assertTrue(nestedObjectProp.value is KsonObject)
+        val nestedObject = nestedObjectProp.value as KsonObject
         val nestedProps = nestedObject.properties.associateBy { it.name.value }
         
-        assertEquals("value1", (nestedProps["key1"]!!.value as StringNodeApi).value)
+        assertEquals("value1", (nestedProps["key1"]!!.value as KsonString).value)
         
-        val deepObject = nestedProps["key2"]!!.value as ObjectNodeApi
+        val deepObject = nestedProps["key2"]!!.value as KsonObject
         val deepProps = deepObject.properties.associateBy { it.name.value }
-        assertEquals("deep value", (deepProps["deepKey"]!!.value as StringNodeApi).value)
+        assertEquals("deep value", (deepProps["deepKey"]!!.value as KsonString).value)
 
         // Verify nested list
         val nestedListProp = properties["nestedList"]!!
-        assertTrue(nestedListProp.value is ListNodeApi)
-        val nestedList = nestedListProp.value as ListNodeApi
+        assertTrue(nestedListProp.value is KsonList)
+        val nestedList = nestedListProp.value as KsonList
         val elements = nestedList.elements.map { it.valueNodeApi }
 
         assertEquals(6, elements.size)
-        assertTrue(elements[0] is StringNodeApi)
-        assertEquals("string element", (elements[0] as StringNodeApi).value)
+        assertTrue(elements[0] is KsonString)
+        assertEquals("string element", (elements[0] as KsonString).value)
         
-        assertTrue(elements[1] is NumberNodeApi)
-        assertEquals(42, ((elements[1] as NumberNodeApi).value as NumberParser.ParsedNumber.Integer).value)
+        assertTrue(elements[1] is KsonNumber)
+        assertEquals(42, ((elements[1] as KsonNumber).value as NumberParser.ParsedNumber.Integer).value)
         
-        assertTrue(elements[2] is BooleanNodeApi)
-        assertEquals(true, (elements[2] as BooleanNodeApi).value)
+        assertTrue(elements[2] is KsonBoolean)
+        assertEquals(true, (elements[2] as KsonBoolean).value)
         
-        assertTrue(elements[3] is NullNodeApi)
+        assertTrue(elements[3] is KsonNull)
         
-        assertTrue(elements[4] is ObjectNodeApi)
-        val objectInList = elements[4] as ObjectNodeApi
+        assertTrue(elements[4] is KsonObject)
+        val objectInList = elements[4] as KsonObject
         val objectInListProps = objectInList.properties.associateBy { it.name.value }
-        assertEquals("value", (objectInListProps["objectInList"]!!.value as StringNodeApi).value)
+        assertEquals("value", (objectInListProps["objectInList"]!!.value as KsonString).value)
         
-        assertTrue(elements[5] is ListNodeApi)
-        val nestedListInList = elements[5] as ListNodeApi
+        assertTrue(elements[5] is KsonList)
+        val nestedListInList = elements[5] as KsonList
         val nestedListElements = nestedListInList.elements.map { it.valueNodeApi }
         assertEquals(3, nestedListElements.size)
-        assertTrue(nestedListElements.all { it is StringNodeApi })
+        assertTrue(nestedListElements.all { it is KsonString })
         assertEquals(
             listOf("nested", "list", "elements"),
-            nestedListElements.map { (it as StringNodeApi).value }
+            nestedListElements.map { (it as KsonString).value }
         )
     }
 } 
