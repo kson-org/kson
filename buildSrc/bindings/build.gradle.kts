5a12d63cf53d892fe5fb168eb0759b1dc2b7a30d
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2025-06-23 17:15:28 -0400

    Target JVM 11 in metadata collector build

--- buildSrc/bindings/build.gradle.kts
+++ buildSrc/bindings/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization") version "2.1.10"
@@ -13,6 +16,20 @@ dependencies {
 
     // Necessary to obtain metadata about Kson's public API through a KSP processor
     implementation("com.google.devtools.ksp:symbol-processing-api:2.1.21-2.0.2")
+}
+
+tasks {
+    val javaVersion = "11"
+    withType<JavaCompile> {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
+    withType<KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.fromTarget(javaVersion))
+        }
+    }
 }
 
 // The KSP processor gets compiled as a JAR because the root build cannot directly depend on
