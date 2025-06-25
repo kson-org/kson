1e39e3bbabab8490a2aac4d33848c6fce485a9d5
Bart Dubbeldam <bartdubbeldam2000@gmail.com>
2025-06-25 10:32:56 -0700

    Refactor BackspaceHandler and TypedHandler
    I realized that `KsonBackspaceHandlerDelegate` overrides the
    `beforeCharTyped` method while the `KsonTypedHandlerDelegate` overrides
    the `charTyped` method. It turns out that using `beforeCharTyped` is
    easier to implement, especially when we need to check whether the caret
    is in a specific element.
    I remembered how confusing it was to work with the caret offset when
    fixing a bug where we auto-completed in a comment (see: aa7bdb35).
    Overriding the `beforeCharTyped` makes this a lot more intuitive.
    
    Another place where we used the `caretOffsets` was to find the character
    that would be deleted or typed. This character is passed as an argument,
    so we don't need the `text[caretOffset]` or `text[caretOffset - 1]`
    anymore. Instead of the `if` statements we use a `when` statement
    matching the typed or deleted character.

--- tooling/jetbrains/src/main/kotlin/org/kson/jetbrains/editor/KsonTypedHandlerDelegate.kt
+++ tooling/jetbrains/src/main/kotlin/org/kson/jetbrains/editor/KsonTypedHandlerDelegate.kt
@@ -3,9 +3,12 @@ package org.kson.jetbrains.editor
 import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiErrorElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.util.childrenOfType
 import org.kson.jetbrains.KsonLanguage
 import org.kson.jetbrains.parser.elem
 import org.kson.jetbrains.util.getIndentType
@@ -16,10 +19,23 @@ import org.kson.parser.ParsedElementType.EMBED_BLOCK
 import org.kson.parser.TokenType.*
 
 class KsonTypedHandlerDelegate : TypedHandlerDelegate() {
-    override fun charTyped(char: Char, project: Project, editor: Editor, file: PsiFile): Result {
+    override fun beforeCharTyped(
+        char: Char,
+        project: Project,
+        editor: Editor,
+        file: PsiFile,
+        fileType: FileType
+    ): Result {
         // this handler runs on typing events in all filetypes, so
         // be careful to return quickly if this event isn't for us
         if (!file.viewProvider.baseLanguage.isKindOf(KsonLanguage)) {
+            return Result.CONTINUE
+        }
+
+        /** If our file contains an error we immediately continue
+         * Indirectly this prevents auto-insertion when we have a dangling '>' or [EMBED_CLOSE_DELIM]
+         **/
+        if (file.childrenOfType<PsiErrorElement>().isNotEmpty()) {
             return Result.CONTINUE
         }
 
@@ -27,67 +43,56 @@ class KsonTypedHandlerDelegate : TypedHandlerDelegate() {
         val document = editor.document
         val text = document.text
 
-        /**
-         * Position before the just-typed character, used for:
-         * 1. Detecting paired delimiters (%% or $$) by matching with the just-typed char
-         * 2. Checking if we're in a comment (since the PSI tree hasn't updated yet for the new character we check
-         *      whether the previous character is a comment)
-         */
-        val preTypedPosition = caretOffset - 2
-
-        // ensure we're not contained in an element where we NEVER want to
-        // auto-complete (e.g. COMMENTS).
-        if (file.hasElementAtOffset(preTypedPosition, autoCompleteProhibitedElems)) {
+        // ensure we're not contained in an element where we NEVER want to auto-complete (e.g. COMMENTS and STRINGS).
+        if (file.hasElementAtOffset(caretOffset - 1, autoCompleteProhibitedElems)) {
             return Result.CONTINUE
         }
 
-        if (CodeInsightSettings.getInstance().AUTOINSERT_PAIR_BRACKET && caretOffset > 0) {
-            /**
-             * Handle auto-inserts of [ANGLE_BRACKET_L]/[ANGLE_BRACKET_R] pairs
-             */
-            if (char == '<') {
-                document.insertString(caretOffset, ">")
-                return Result.CONTINUE
-            }
+        if (CodeInsightSettings.getInstance().AUTOINSERT_PAIR_BRACKET) {
+            when (char) {
+                /**
+                 * Handle auto-inserts of [ANGLE_BRACKET_L]/[ANGLE_BRACKET_R] pairs
+                 */
+                '<' -> {
+                    document.insertString(caretOffset, ">")
+                    return Result.CONTINUE
+                }
 
-            /**
-             * Handle auto-inserts of [EMBED_OPEN_DELIM]/[EMBED_CLOSE_DELIM] pairs
-             */
-            if (text.length > 1 && caretOffset > 1) {
-                for (embedDelimChar in listOf(EmbedDelim.Percent.char, EmbedDelim.Dollar.char)) {
-                    if (char == embedDelimChar && text[preTypedPosition] == embedDelimChar) {
-                        // let's be very conservative with this insert: if we are not at the end of a line,
-                        //     or followed by a comma (which is end of line-ish), don't do it
-                        if (text.length != caretOffset
-                            && text[caretOffset] != '\n'
-                            && text[caretOffset] != ',') {
-                            return Result.CONTINUE
-                        }
-
-                        // ensure we're not contained in an element where embed block delimiter
-                        // auto-insertions would be inappropriate
-                        if (file.hasElementAtOffset(preTypedPosition, embedInsertProhibitedElems)) {
-                            return Result.CONTINUE
-                        }
-
-                        // Insert the new line on which we auto-complete the [EmbedDelim]
-                        document.insertString(caretOffset, "\n")
-
-                        // Calculate the indent level of the inserted new line
-                        val newLineCaret = caretOffset + 1
-                        val lineNumber = document.getLineNumber(newLineCaret)
-                        val indentType = file.getIndentType()
-                        val indentLevel = document.getLineIndentLevel(lineNumber, indentType)
-                        val newIndent = indentType.indentString.repeat(indentLevel)
-
-                        // Insert the embed delimiters with the calculated indent
-                        document.insertString(newLineCaret, "${newIndent}$embedDelimChar$embedDelimChar")
+                /**
+                 * Handle auto-inserts of [EMBED_OPEN_DELIM]/[EMBED_CLOSE_DELIM] pairs
+                 */
+                EmbedDelim.Dollar.char, EmbedDelim.Percent.char -> {
+                    // let's be very conservative with this insert: if we are not at the end of a line,
+                    //     or followed by a comma (which is end of line-ish), don't do it
+                    if (text.length != caretOffset
+                        && text[caretOffset] != '\n'
+                        && text[caretOffset] != ',') {
                         return Result.CONTINUE
                     }
+
+                    // ensure we're not contained in an element where embed block delimiter
+                    // auto-insertions would be inappropriate
+                    if (file.hasElementAtOffset(caretOffset, embedInsertProhibitedElems)) {
+                        return Result.CONTINUE
+                    }
+
+                    // Insert the new line on which we auto-complete the [EmbedDelim]
+                    document.insertString(caretOffset, "\n")
+
+                    // Calculate the indent level of the inserted new line
+                    val newLineCaret = caretOffset + 1
+                    val lineNumber = document.getLineNumber(newLineCaret)
+                    val indentType = file.getIndentType()
+                    val indentLevel = document.getLineIndentLevel(lineNumber, indentType)
+                    val newIndent = indentType.indentString.repeat(indentLevel)
+
+                    // Insert the embed delimiters with the calculated indent
+                    val embedDelim = EmbedDelim.fromString(char.toString())
+                    document.insertString(newLineCaret, "${newIndent}${embedDelim.closeDelimiter}")
+                    return Result.CONTINUE
                 }
             }
         }
-
         return Result.CONTINUE
     }
 }
