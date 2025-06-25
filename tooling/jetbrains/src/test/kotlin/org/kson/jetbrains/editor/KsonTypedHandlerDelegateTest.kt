db41eccd498bc8378d4a08af8e2beea694f2ae0a
Bart Dubbeldam <bartdubbeldam2000@gmail.com>
2025-06-25 10:35:10 -0700

    Tests for no string auto-completion `<`
    Commit ac23ff1aa3fbda99c4dcf93af7ac7c9c5049a4f4 fixed a bug
    where we mistakenly auto-completed inside strings. This commit adds test
    to prevent the same bug from happening in the future.

--- tooling/jetbrains/src/test/kotlin/org/kson/jetbrains/editor/KsonTypedHandlerDelegateTest.kt
+++ tooling/jetbrains/src/test/kotlin/org/kson/jetbrains/editor/KsonTypedHandlerDelegateTest.kt
@@ -42,6 +42,20 @@ class KsonTypedHandlerDelegateTest : KsonEditorActionTest() {
                 '<',
                 "#<<caret>"
             )
+
+            // should not auto-insert in a string
+            doCharTest(
+                "\"<caret>\"",
+                '<',
+                "\"<<caret>\""
+            )
+
+            // should not auto-insert in a string
+            doCharTest(
+                "\"  <caret>\"",
+                '<',
+                "\"  <<caret>\""
+            )
         }
 
         withConfigSetting(ConfigProperty.AUTOINSERT_PAIR_BRACKET(), false) {
@@ -135,6 +149,17 @@ class KsonTypedHandlerDelegateTest : KsonEditorActionTest() {
                     """.trimIndent(),
                 )
 
+                doCharTest(
+                    """
+                    "  <caret>
+                    "
+                    """.trimIndent(),
+                    openDelim,
+                    """
+                    "  $openDelim<caret>
+                    "
+                    """.trimIndent(),
+                )
                 // should not auto-insert inside other embeds
                 doCharTest(
                     """
