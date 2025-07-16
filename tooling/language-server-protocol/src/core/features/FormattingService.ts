688c36372de73c56b94d971d8c078165ee3b95bc
Bart Dubbeldam <bartdubbeldam2000@gmail.com>
2025-07-16 15:12:21 -0700

    Update LSP for new `FormattingStyle`

--- tooling/language-server-protocol/src/core/features/FormattingService.ts
+++ tooling/language-server-protocol/src/core/features/FormattingService.ts
@@ -1,6 +1,6 @@
 import {TextEdit, FormattingOptions} from 'vscode-languageserver';
 import {KsonDocument} from '../document/KsonDocument.js';
-import {FormatOptions, Kson} from 'kson';
+import {FormatOptions, IndentType, Kson} from 'kson';
 
 /**
  * Service responsible for formatting Kson documents
@@ -9,8 +9,8 @@ export class FormattingService {
 
     formatDocument(document: KsonDocument, options: FormattingOptions): TextEdit[] {
         const indentType: FormatOptions = options.insertSpaces
-            ? new FormatOptions.Spaces(options.tabSize)
-            : FormatOptions.Tabs;
+            ? new FormatOptions(new IndentType.Spaces(options.tabSize))
+            : new FormatOptions(IndentType.Tabs);
 
         const formattedKson = Kson.getInstance().format(
             document.textDocument.getText(),
