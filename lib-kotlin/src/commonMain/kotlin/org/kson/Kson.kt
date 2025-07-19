e02672385011e995c338126215bdd7fc174ef504
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2025-07-18 22:45:22 -0400

    Add SimpleListIterator to `lib-kotlin`
    This class is necessary to allow FFI users to iterate through lists
    returned by kotlin. It is not _really_ part of the public API, but there
    is no better place for it right now. Maybe we could introduce a
    `lib-native` that exposes everything from `lib-kotlin` and adds the
    `SimpleListIterator` type on top, to keep things cleaner.

--- lib-kotlin/src/commonMain/kotlin/org/kson/Kson.kt
+++ lib-kotlin/src/commonMain/kotlin/org/kson/Kson.kt
@@ -415,3 +415,18 @@ private fun createPublicToken(publicTokenType: TokenType, internalToken: Interna
         Position(internalToken.lexeme.location.end)
     )
 }
+
+/**
+ * Helper class to let FFI users iterate through the elements of a [List]
+ */
+sealed class SimpleListIterator(list: List<Any>) {
+    private val inner = list.iterator()
+
+    fun next(): Any? {
+        return if (inner.hasNext()) {
+            inner.next()
+        } else {
+            null
+        }
+    }
+}
