8c0e9a57643c33b848344cde270af7e0a2043e37
Bart Dubbeldam <bartdubbeldam2000@gmail.com>
2025-08-02 11:54:46 -0700

    Fix bug empty objects
    We ran into a bug with empty objects when using the compact formatting
    style due to the use of `first()` on an empty list. This has been
    rewritten to be null safe.

--- src/commonMain/kotlin/org/kson/ast/Ast.kt
+++ src/commonMain/kotlin/org/kson/ast/Ast.kt
@@ -363,14 +363,15 @@ class ObjectPropertyNodeImpl(
 
     private fun compactObjectProperty(indent: Indent, nextNode: AstNode?, compileTarget: CompileTarget): String {
         // A comment always needs to start on a new line. This can happen either when the first property or the next
-        // node are commented.
-        val firstObjectPropertyHasComments =
-            (value is ObjectNode) && (value.properties.first() is ObjectPropertyNodeImpl)
-                    && ((value.properties.first() as ObjectPropertyNodeImpl).comments.isNotEmpty())
+        // node is commented.
+        val firstPropertyIsObjectWithComments = (value as? ObjectNode)
+            ?.properties?.firstOrNull()?.let { prop ->
+                (prop as? ObjectPropertyNodeImpl)?.comments?.isNotEmpty() == true
+            } ?: false
         val nextNodeHasComments = (nextNode is Documented && nextNode.comments.isNotEmpty())
 
         return key.toSourceWithNext(indent, value, compileTarget) +
-                if (firstObjectPropertyHasComments) {
+                if (firstPropertyIsObjectWithComments) {
                     "\n"
                 } else {
                     ""
