6653dd5edbbb203cc450c348ab318f7db484d44f
Daniel Marcotte <daniel@marcottemotors.io>
2025-09-17 15:46:06 +0200

    Update versions for development of next release

--- kson-lib/build.gradle.kts
+++ kson-lib/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 group = "org.kson"
-version = "0.1.1"
+version = "0.1.2-SNAPSHOT"
 
 tasks {
     val copyHeaderDynamic = register<CopyNativeHeaderTask>("copyNativeHeaderDynamic") {
