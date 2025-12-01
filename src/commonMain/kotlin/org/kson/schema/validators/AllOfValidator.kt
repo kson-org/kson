92662c9112f1afbe79d57770d747f03a64668fba
Daniel Marcotte <daniel@marcottemotors.io>
2025-12-01 17:21:20 -0300

    Fix typo

--- src/commonMain/kotlin/org/kson/schema/validators/AllOfValidator.kt
+++ src/commonMain/kotlin/org/kson/schema/validators/AllOfValidator.kt
@@ -7,7 +7,7 @@ import org.kson.schema.JsonSchemaValidator
 
 class AllOfValidator(val allOf: List<JsonSchema>) : JsonSchemaValidator {
     override fun validate(ksonValue: KsonValue, messageSink: MessageSink) {
-        // log and and all error we see from this collection of schemas we must satisfy
+        // log any and all error we see from this collection of schemas we must satisfy
         allOf.forEach {
             it.validate(ksonValue, messageSink)
         }
